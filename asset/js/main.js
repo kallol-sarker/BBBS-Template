370ba88c3e7130639fe13c33a2bb4d085b74af0e
kallol <kallolsarker72@gmail.com>
2021-12-04 12:04:07 +0600

    BBBS Template Commited

---
+++ asset/js/main.js
@@ -0,0 +1,43 @@
+// $(document).ready(function(){
+    
+//       var isMenuOpen = false;
+  
+//       $("#click-btn").click(function () {
+//           if (isMenuOpen == false) {
+//               $("#nav-menu").slideDown("1000");
+//               isMenuOpen = true;
+//           } else {
+//               $("#nav-menu").slideUp("1000");
+//               isMenuOpen = false;
+//           }
+//       });
+  
+//       $(window).resize(function () {
+//           var screenSize = $(window).width();
+  
+//           if (screenSize > 576) {
+//               $("#nav-menu").removeAttr("style");
+//           }
+//       });
+      
+//       var isSearchBarOpen = false;
+  
+//       $("#search-btn").click(function () {
+//           if (isMenuOpen == false) {
+//               $("#search-bar").slideDown("1000");
+//               isMenuOpen = true;
+//           } else {
+//               $("#search-bar").slideUp("1000");
+//               isMenuOpen = false;
+//           }
+//       });
+  
+//       $(window).resize(function () {
+//           var screenSize = $(window).width();
+  
+//           if (screenSize > 576) {
+//               $("#search-bar").removeAttr("style");
+//           }
+//       });
+      
+//   });
